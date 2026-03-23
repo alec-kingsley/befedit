efdcd0fa163e358ff72f9ab3d06d8d833c58a8ea
Alec Kingsley <aleckingsley@gmail.com>
2026-03-23 00:23:35 -0400

    Update reporter to account for terminal settings

--- src/reporter.c
+++ src/reporter.c
@@ -4,19 +4,23 @@
 #include <stdlib.h>
 
 void report_system_error(const char *error) {
-    fprintf(stderr, RED "SYSTEM ERROR: " RESET "%s\n", error);
+    printf(CLEAR_SCREEN RESET_CURSOR SHOW_CURSOR);
+    fprintf(stderr, RED "SYSTEM ERROR: " RESET "%s\r\n", error);
 }
 
 void report_logic_error(const char *error) {
-    fprintf(stderr, RED "LOGIC ERROR: " RESET "%s\n", error);
+    printf(CLEAR_SCREEN RESET_CURSOR SHOW_CURSOR);
+    fprintf(stderr, RED "LOGIC ERROR: " RESET "%s\r\n", error);
     /* not safe to continue */
     exit(1);
 }
 
 void report_error(const char *error) {
-    printf(RED "ERROR: " RESET "%s\n", error);
+    printf(CLEAR_SCREEN RESET_CURSOR SHOW_CURSOR);
+    printf(RED "ERROR: " RESET "%s\r\n", error);
 }
 
 void report_warning(const char *error) {
-    printf(ORANGE "WARNING: " RESET "%s\n", error);
+    printf(CLEAR_SCREEN RESET_CURSOR SHOW_CURSOR);
+    printf(ORANGE "WARNING: " RESET "%s\r\n", error);
 }
